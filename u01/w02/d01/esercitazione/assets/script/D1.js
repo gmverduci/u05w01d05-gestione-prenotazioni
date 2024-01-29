88c02489606efd552c2d764ce32d7344cbe6d442
Marcello Verduci <156176363+gmverduci@users.noreply.github.com>
2024-01-29 17:31:51 +0100

    formattato file js

--- u01/w02/d01/esercitazione/assets/script/D1.js
+++ u01/w02/d01/esercitazione/assets/script/D1.js
@@ -19,22 +19,27 @@ REGOLE
 - undefined:
 */
 
-let z = 'Principali datatype - ';
+let z = "Principali datatype - ";
 
-let a = ' Stringa: ';
-let a2 = 'Dati indicati sempre tra apici che vengono trattati come testo. Quindi anche indicando un numero tra gli apici, qualora si eseguisse un\'\operazione di somma il risultato non sarebbe la somma tra i due numeri ma una concatenazione tra la stringa e il numero che volevamo sommare. ------';
+let a = " Stringa: ";
+let a2 =
+  "Dati indicati sempre tra apici che vengono trattati come testo. Quindi anche indicando un numero tra gli apici, qualora si eseguisse un'operazione di somma il risultato non sarebbe la somma tra i due numeri ma una concatenazione tra la stringa e il numero che volevamo sommare. ------";
 
-let b = ' Numero: ';
-let b2 = 'Valore numerico classico che può essere utilizzato per operazione di calcolo matematico. Si indica senza apici al contrario delle stringhe. Visto che in Javascript è un linguaggio a tipizzazione debole, potremo indicare direttamente un numero intero o decimale. ------'
+let b = " Numero: ";
+let b2 =
+  "Valore numerico classico che può essere utilizzato per operazione di calcolo matematico. Si indica senza apici al contrario delle stringhe. Visto che in Javascript è un linguaggio a tipizzazione debole, potremo indicare direttamente un numero intero o decimale. ------";
 
-let c = ' Boolean: ';
-let c2 = 'Un dato è di tipo booleano se può dare come risultato solo vero o falso, quindi ha esclusivamente valore binario. E\'\ utile per la verifica o l\'\inserimento di una condizione. ------';
+let c = " Boolean: ";
+let c2 =
+  "Un dato è di tipo booleano se può dare come risultato solo vero o falso, quindi ha esclusivamente valore binario. E' utile per la verifica o l'inserimento di una condizione. ------";
 
-let d = ' Null: ';
-let d2 = 'è un valore nullo, segnala cioè l\'\assenza di un oggetto. Al contrario di 0, che in informatica è il primo dei numeri positivi, indica una mancanza di identificazione e nella logica booleana rappresenta una condizione false. E\'\ utile per identificare problemi di funzionamento degli script. ------';
+let d = " Null: ";
+let d2 =
+  "è un valore nullo, segnala cioè l'assenza di un oggetto. Al contrario di 0, che in informatica è il primo dei numeri positivi, indica una mancanza di identificazione e nella logica booleana rappresenta una condizione false. E' utile per identificare problemi di funzionamento degli script. ------";
 
-let e = ' undefined: ';
-let e2 = 'indica che non è stato assegnato un valore a una variabile. Se ad esempio proviamo a usare una variabile non precedentemente definita otterremo un avviso di undefined.';
+let e = " undefined: ";
+let e2 =
+  "indica che non è stato assegnato un valore a una variabile. Se ad esempio proviamo a usare una variabile non precedentemente definita otterremo un avviso di undefined.";
 
 let Risposta1 = z + a + a2 + b + b2 + c + c2 + d + d2 + e + e2;
 console.log(Risposta1);
@@ -43,9 +48,8 @@ console.log(Risposta1);
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-let myName = 'Marcello'
+let myName = "Marcello";
 console.log(myName);
-
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
@@ -60,17 +64,15 @@ console.log(12 + 20);
 let x = 12;
 console.log(x);
 
-
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-myName = 'Verduci';
+myName = "Verduci";
 
 /* const mySurname = 'Verduci';
-const mySurname = 'Martino'; */
-
+mySurname = 'Martino'; */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
@@ -78,15 +80,14 @@ const mySurname = 'Martino'; */
 
 console.log(4 - x);
 
-
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-let name1 = 'john';
-let name2 = 'John';
+let name1 = "john";
+let name2 = "John";
 console.log(name1 != name2);
 
 console.log(name1.toLowerCase() === name2.toLowerCase());
