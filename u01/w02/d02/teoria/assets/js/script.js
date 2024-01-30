e90ff299764f8bbe839a49f1018aa28a11fb2a00
Marcello Verduci <156176363+gmverduci@users.noreply.github.com>
2024-01-30 18:24:02 +0100

    Esercitazione u01 w02 d02

---
+++ u01/w02/d02/teoria/assets/js/script.js
@@ -0,0 +1,11 @@
+const studente = {
+  nome: "Mario",
+  cognome: "Rossi",
+  eta: 25,
+  classe: "FS0124",
+  webcam: false,
+};
+
+console.log(studente);
+
+document.getElementById("oggetto").innerHTML = studente;
