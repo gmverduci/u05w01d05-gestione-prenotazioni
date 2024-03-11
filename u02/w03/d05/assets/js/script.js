0d552aaed7f576cfd0feb73db2f34d47f5f40238
Marcello Verduci <156176363+gmverduci@users.noreply.github.com>
2024-03-11 01:02:51 +0100

    progetto settimanale

--- u02/w03/d05/assets/js/script.js
+++ u02/w03/d05/assets/js/script.js
@@ -3,10 +3,28 @@ const apiKey =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZDgwMjJkN2IxMTAwMTkwZTZkZGEiLCJpYXQiOjE3MDk4ODk1MzgsImV4cCI6MTcxMTA5OTEzOH0.lxPP1pvzX_otJ2Lm1nttNsifxs64JKd_VW8f3QEVdVg";
 let productsData = [];
 
+const logInBtn = document.getElementById("logInBtn");
+const backendBtn = document.getElementById("backendBtn");
+const homeLink = document.getElementById("homeLink");
+
 function init() {
   console.log("Initialization started");
   buildPage();
   fetchProducts();
+}
+
+function afterLogin() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (isLoggedIn === "true") {
+    const backendTabContainer = document.querySelector(
+      ".backend-tab-container"
+    );
+    const editButtons = document.querySelectorAll(".edit-button");
+    backendTabContainer.classList.remove("d-none");
+    editButtons.forEach((button) => {
+      button.classList.remove("d-none");
+    });
+  }
 }
 
 function buildPage() {
@@ -26,8 +44,8 @@ function buildPage() {
     "navbar-brand d-flex justify-content-start align-items-between";
   navbarBrand.href = "#";
   navbarBrand.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-controller me-2" viewBox="0 0 16 16">
-    <path d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1z"/>
-    <path d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729q.211.136.373.297c.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466s.34 1.78.364 2.606c.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527s-2.496.723-3.224 1.527c-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.3 2.3 0 0 1 .433-.335l-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a14 14 0 0 0-.748 2.295 12.4 12.4 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.4 12.4 0 0 0-.339-2.406 14 14 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27s-2.063.091-2.913.27"/></svg><h1 class="h4 mb-0">Marcello's Games</h1 class="h4">`;
+       <path d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1z"/>
+       <path d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729q.211.136.373.297c.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466s.34 1.78.364 2.606c.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527s-2.496.723-3.224 1.527c-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.3 2.3 0 0 1 .433-.335l-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a14 14 0 0 0-.748 2.295 12.4 12.4 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.4 12.4 0 0 0-.339-2.406 14 14 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27s-2.063.091-2.913.27"/></svg><h1 class="h4 mb-0">Marcello's Games</h1 class="h4">`;
   navbar.appendChild(navbarBrand);
 
   navbarBrand.addEventListener("click", async () => {
@@ -47,39 +65,21 @@ function buildPage() {
   btnCollpase.appendChild(collapseToggle);
 
   const navbarNav = document.createElement("div");
-  navbarNav.className = "collapse navbar-collapse";
+  navbarNav.className = "collapse navbar-collapse justify-content-end";
   navbarNav.id = "navbarDropdown";
   navbar.appendChild(navbarNav);
 
-  const navbarNavUl = document.createElement("ul");
-  navbarNavUl.className = "navbar-nav";
-  navbarNav.appendChild(navbarNavUl);
+  const navbarNavUlRight = document.createElement("ul");
+  navbarNavUlRight.className = "navbar-nav ms-auto";
+  navbarNav.appendChild(navbarNavUlRight);
 
-  const navbarNavLiHome = document.createElement("li");
-  navbarNavLiHome.className = "nav-item active";
-  navbarNavUl.appendChild(navbarNavLiHome);
-
-  const navbarNavLiHomeA = document.createElement("a");
-  navbarNavLiHomeA.className = "nav-link";
-  navbarNavLiHomeA.href = "#";
-  navbarNavLiHomeA.textContent = "Home";
-  navbarNavLiHomeA.addEventListener("click", async () => {
-    await fetchProducts();
-    showHomePage(productsData);
-  });
-
-  navbarNavLiHome.appendChild(navbarNavLiHomeA);
-
-  const navbarNavLiBackOffice = document.createElement("li");
-  navbarNavLiBackOffice.className = "nav-item";
-  navbarNavUl.appendChild(navbarNavLiBackOffice);
-
-  //   const navbarNavLiBackOfficeA = document.createElement("a");
-  //   navbarNavLiBackOfficeA.className = "nav-link";
-  //   navbarNavLiBackOfficeA.href = "#";
-  //   navbarNavLiBackOfficeA.textContent = "Back-Office";
-  //   navbarNavLiBackOfficeA.addEventListener("click", () => showBackOfficePage());
-  //   navbarNavLiBackOffice.appendChild(navbarNavLiBackOfficeA);
+  const logInLink = document.createElement("a");
+  logInLink.className = "nav-link bg-secondary text-white rounded";
+  logInLink.href = "#";
+  logInLink.id = logInBtn;
+  logInLink.textContent = "Login";
+  logInLink.addEventListener("click", () => showLogInModal());
+  navbarNavUlRight.appendChild(logInLink);
 
   const main = document.createElement("main");
   main.className = "container my-3";
@@ -113,7 +113,7 @@ function showHomePage(products) {
 
   const backendTabContainer = document.createElement("div");
   backendTabContainer.className =
-    "d-flex flex-column align-items-end position-fixed top-30 end-0 me-5 mt-1 pe-5";
+    "d-flex flex-column align-items-end position-fixed top-30 end-0 me-5 mt-1 pe-5 backend-tab-container d-none";
   backendTabContainer.style.zIndex = "1000";
   main.appendChild(backendTabContainer);
 
@@ -123,7 +123,7 @@ function showHomePage(products) {
   backendTabButton.setAttribute("data-bs-target", "#backendTabContent");
   backendTabButton.setAttribute("aria-controls", "backendTabContent");
   backendTabButton.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
 </svg>
@@ -178,7 +178,7 @@ function showHomePage(products) {
 
       const cardPrice = document.createElement("p");
       cardPrice.className = "card-text fs-5";
-      cardPrice.innerHTML = `Prezzo: € ${product.price}.00`; /* A dimostrazione della mia enorme forza di volontà ho inserito .00 al posto di un Math.random */
+      cardPrice.innerHTML = `Price: € ${product.price}.00`; /* A dimostrazione della mia enorme forza di volontà ho inserito .00 al posto di un Math.random */
       cardBody.appendChild(cardPrice);
 
       const cardFooter = document.createElement("div");
@@ -192,7 +192,7 @@ function showHomePage(products) {
       productCard.appendChild(cardButtons);
 
       const editButton = document.createElement("button");
-      editButton.className = "btn btn-warning px-3 me-4";
+      editButton.className = "btn btn-warning px-3 me-4 edit-button d-none";
       editButton.textContent = "Edit";
       editButton.addEventListener("click", () => showEditProductPage(product));
       cardButtons.appendChild(editButton);
@@ -208,6 +208,123 @@ function showHomePage(products) {
   } else {
     console.error("Products data is not an array:", products);
   }
+
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (isLoggedIn === "true") {
+    const backendTabContainer = document.querySelector(
+      ".backend-tab-container"
+    );
+    const editButtons = document.querySelectorAll(".edit-button");
+    backendTabContainer.classList.remove("d-none");
+    editButtons.forEach((button) => {
+      button.classList.remove("d-none");
+    });
+  }
+  homeLink.style.display = "block";
+}
+
+function showLogInModal() {
+  const modal = document.createElement("div");
+  modal.id = "logInModal";
+  modal.className = "modal fade";
+  modal.tabIndex = "-1";
+  modal.setAttribute("aria-labelledby", "logInModalLabel");
+  modal.setAttribute("aria-hidden", "true");
+  document.body.appendChild(modal);
+
+  const modalDialog = document.createElement("div");
+  modalDialog.className = "modal-dialog";
+  modal.appendChild(modalDialog);
+
+  const modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  modalDialog.appendChild(modalContent);
+
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+  modalContent.appendChild(modalHeader);
+
+  const modalTitle = document.createElement("h5");
+  modalTitle.className = "modal-title";
+  modalTitle.id = "logInModalLabel";
+  modalTitle.textContent = "Log In";
+  modalHeader.appendChild(modalTitle);
+
+  const closeButton = document.createElement("button");
+  closeButton.type = "button";
+  closeButton.className = "btn-close";
+  closeButton.setAttribute("data-bs-dismiss", "modal");
+  modalHeader.appendChild(closeButton);
+
+  const modalBody = document.createElement("div");
+  modalBody.className = "modal-body";
+  modalContent.appendChild(modalBody);
+
+  const usernameLabel = document.createElement("label");
+  usernameLabel.htmlFor = "usernameInput"; 
+  usernameLabel.textContent = "Username";
+  modalBody.appendChild(usernameLabel);
+
+  const usernameInput = document.createElement("input");
+  usernameInput.type = "text";
+  usernameInput.id = "usernameInput"; 
+  usernameInput.className = "form-control mb-2";
+  usernameInput.value = "admin";
+  usernameInput.readOnly = true;
+  modalBody.appendChild(usernameInput);
+
+  const passwordLabel = document.createElement("label");
+  passwordLabel.htmlFor = "passwordInput"; 
+  passwordLabel.textContent = "Password";
+  modalBody.appendChild(passwordLabel);
+
+  const passwordInput = document.createElement("input");
+  passwordInput.type = "password";
+  passwordInput.id = "passwordInput"; 
+  passwordInput.className = "form-control";
+  modalBody.appendChild(passwordInput);
+
+  const passwordHint = document.createElement("p");
+  passwordHint.className = "text-danger";
+  passwordHint.style.fontSize = "0.8em";
+  passwordHint.textContent =
+    "Your password is the most famous piece from Brahms.";
+  modalBody.appendChild(passwordHint);
+
+  const modalFooter = document.createElement("div");
+  modalFooter.className = "modal-footer";
+  modalContent.appendChild(modalFooter);
+
+  const guestAccessButton = document.createElement("button");
+  guestAccessButton.type = "button";
+  guestAccessButton.className = "btn btn-secondary";
+  guestAccessButton.textContent = "Guest Access";
+  guestAccessButton.setAttribute("data-bs-dismiss", "modal");
+  modalFooter.appendChild(guestAccessButton);
+
+  const adminLoginButton = document.createElement("button");
+  adminLoginButton.type = "button";
+  adminLoginButton.className = "btn btn-primary";
+  adminLoginButton.textContent = "Admin Login";
+  adminLoginButton.setAttribute("data-bs-dismiss", "modal");
+  adminLoginButton.addEventListener("click", () => {
+    const password = passwordInput.value;
+    if (
+      password === "ninnananna" ||
+      password === "lullaby" ||
+      password === "cradle song"
+    ) {
+      localStorage.setItem("isLoggedIn", "true");
+
+      afterLogin();
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  });
+  modalFooter.appendChild(adminLoginButton);
+
+  const bsModal = new bootstrap.Modal(modal);
+  bsModal.show();
 }
 
 function showEditProductPage(product) {
@@ -216,7 +333,7 @@ function showEditProductPage(product) {
 
   const backendTabContainer = document.createElement("div");
   backendTabContainer.className =
-    "d-flex flex-column align-items-end position-fixed top-30 end-0 me-5 mt-1 pe-5";
+    "d-flex flex-column align-items-end position-fixed top-10 end-0";
   backendTabContainer.style.zIndex = "1000";
   main.appendChild(backendTabContainer);
 
@@ -226,7 +343,7 @@ function showEditProductPage(product) {
   backendTabButton.setAttribute("data-bs-target", "#backendTabContent");
   backendTabButton.setAttribute("aria-controls", "backendTabContent");
   backendTabButton.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
 </svg>
 `;
@@ -385,7 +502,7 @@ function showProductDetailsPage(product) {
 
   const backendTabContainer = document.createElement("div");
   backendTabContainer.className =
-    "d-flex flex-column align-items-end position-fixed top-30 end-0 me-5 mt-1 pe-5";
+    "d-flex flex-column align-items-end position-fixed top-10 end-0";
   backendTabContainer.style.zIndex = "1000";
   main.appendChild(backendTabContainer);
 
@@ -395,7 +512,7 @@ function showProductDetailsPage(product) {
   backendTabButton.setAttribute("data-bs-target", "#backendTabContent");
   backendTabButton.setAttribute("aria-controls", "backendTabContent");
   backendTabButton.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
 </svg>
 `;
@@ -405,7 +522,7 @@ function showProductDetailsPage(product) {
   });
 
   const detailsContainer = document.createElement("div");
-  detailsContainer.className = "row d-flex align-items-center";
+  detailsContainer.className = "row d-flex align-items-center pt-5";
   main.appendChild(detailsContainer);
 
   const imageContainer = document.createElement("div");
@@ -573,6 +690,7 @@ function showBackOfficePage() {
       console.error("Error adding product:", error);
     }
   });
+  homeLink.style.display = "none";
 }
 
 window.addEventListener("load", init);
