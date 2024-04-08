bbdf5fff151e2184ad71b89f9f42e8c46c145fe3
Marcello Verduci <156176363+gmverduci@users.noreply.github.com>
2024-04-08 17:35:32 +0200

    progetto settimanale movies

--- u03/w03/d05/movies/src/app/components/movies/movies.component.ts
+++ u03/w03/d05/movies/src/app/components/movies/movies.component.ts
@@ -5,69 +5,103 @@ import { UsersService } from 'src/app/services/users.service';
 import { MoviesService } from 'src/app/services/movies.service';
 import { environment } from 'src/environments/environment.development';
 import { AuthService } from 'src/app/auth/auth.service';
-import { ChunkPipe } from 'src/app/pipes/chunk.pipe';
 
 @Component({
-    selector: 'app-movies',
-    templateUrl: './movies.component.html',
-    styleUrls: ['./movies.component.scss'],
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-    user!: Partial<User>;
-    advMovies!: Movie[];
-    allMovies!: Movie[];
-    recommendedMovies!: Movie[];
-    popularMovies!: Movie[];
-    posterURL = environment.posterURL
+  user!: Partial<User>;
+  allMovies!: Movie[];
+  recommendedMovies!: Movie[];
+  popularMovies!: Movie[];
+  posterURL = environment.posterURL;
 
-    constructor(
-        private usersSrv: UsersService,
-        private moviesSrv: MoviesService,
-        private authSrv: AuthService
-    ) {}
+  constructor(
+    private usersSrv: UsersService,
+    private moviesSrv: MoviesService,
+    private authSrv: AuthService
+  ) {}
 
-    ngOnInit(): void {
-        this.authSrv.user$.subscribe((auth) => {
-            if (auth && auth.user) {
-                this.user = auth.user;
-                this.fetchMovies();
-            }
-        });
-    }
-
-    fetchMovies() {
-        this.moviesSrv.getMovies().subscribe((data) => {
-            this.allMovies = data;
-            console.log('All Movies:', this.allMovies);
-
-            if (this.user.adult === false) {
-                this.allMovies = this.allMovies.filter(
-                    (movie) => movie.adult === false
-                );
-            }
-
-            this.recommendedMovies = this.allMovies.filter((movie) =>
-                movie.genre_ids.some((genreId) =>
-                    this.user?.genres?.includes(genreId)
-                )
-            );
-            console.log('Recommended Movies:', this.recommendedMovies);
-            const minVoteAverage = 7;
-            const minVoteCount = 100;
-
-            this.popularMovies = this.allMovies.filter(
-                (movie) =>
-                    movie.vote_average >= minVoteAverage &&
-                    movie.vote_count >= minVoteCount
-            );
-            console.log('Popular Movies:', this.popularMovies);
-        });
-    }
-
-    getImageUrl(posterPath: string | null): string {
-      if (!posterPath) {
-        return 'assets/placeholder-image.jpg'; // Placeholder image URL
+  ngOnInit(): void {
+    this.authSrv.user$.subscribe((auth) => {
+      if (auth && auth.user) {
+        this.user = auth.user;
+        this.fetchMovies();
       }
-      return `${this.posterURL}${posterPath}`;
+    });
+  }
+
+  fetchMovies() {
+    this.moviesSrv.getMovies().subscribe((data) => {
+      this.allMovies = data;
+      console.log('All Movies:', this.allMovies);
+
+      if (this.user.adult === false) {
+        this.allMovies = this.allMovies.filter(
+          (movie) => movie.adult === false
+        );
+      }
+
+      const minVoteAverage = 7;
+      const minVoteCount = 100;
+
+      this.popularMovies = this.getUniqueMovies(
+        this.allMovies.filter(
+          (movie) =>
+            movie.vote_average >= minVoteAverage &&
+            movie.vote_count >= minVoteCount
+        )
+      );
+      console.log('Popular Movies:', this.popularMovies);
+
+     
+      this.recommendedMovies = this.getUniqueMovies(
+        this.allMovies
+          .filter((movie) =>
+            movie.genre_ids.some((genreId) =>
+              this.user?.genres?.includes(genreId)
+            )
+          )
+          .filter(
+            (movie) => !this.popularMovies.some((popMovie) => popMovie.id === movie.id)
+          )
+      );
+      console.log('Recommended Movies:', this.recommendedMovies);
+
+     
+      this.allMovies = this.getUniqueMovies(
+        this.allMovies
+          .filter(
+            (movie) =>
+              !this.recommendedMovies.some(
+                (recMovie) => recMovie.id === movie.id
+              ) && !this.popularMovies.some((popMovie) => popMovie.id === movie.id)
+          )
+      );
+      console.log('All Movies after filtering:', this.allMovies);
+    });
+  }
+
+  getUniqueMovies(movies: Movie[]): Movie[] {
+    const uniqueMovies: Movie[] = [];
+    const movieIds: number[] = [];
+
+    for (const movie of movies) {
+      if (!movieIds.includes(movie.id)) {
+        uniqueMovies.push(movie);
+        movieIds.push(movie.id);
+      }
     }
+
+    return uniqueMovies.slice(0, 8); 
+  }
+
+  getImageUrl(posterPath: string | null): string {
+    if (!posterPath) {
+      return 'assets/placeholder-image.jpg'; 
+    }
+    return `${this.posterURL}${posterPath}`;
+  }
 }
